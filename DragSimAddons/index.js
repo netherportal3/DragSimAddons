261a66e1052153619be73db5f71153ea4c5b8e8f
netherportal3 <151792930+netherportal3@users.noreply.github.com>
2023-12-24 02:37:30 -0600

    Update index.js

--- DragSimAddons/index.js
+++ DragSimAddons/index.js
@@ -1,13 +1,14 @@
-import PogObject from "PogData";
-const persistentData = new PogObject("DragSimAddons", {
-  LootNumLower: 50,
-  backstabStatus: true,
-});
+import Settings from "./config";
+
 var zealotsSinceEye = 0
 var Uncertainty = 0
-//lootnum simplifier
+let dragLootNumCheck = false
+
 register("chat", (message, event) => {
-  if (message.includes("LootNum:")) {
+  if (message.includes("Drag Score: ")&& message.includes("Eyes Placed: ")) {
+    dragLootNumCheck = true
+  }
+  if (message.includes("LootNum:") && Settings.LootNumToggle) {
       cancel(event)
       const regex = /LootNum: (\d+(?:.\d+)?(?:E[+-]?\d+)?) Magic Find Multiplier: (\d+(?:.\d+)?(?:E[+-]?\d+)?)x/g;
       let match;
@@ -15,69 +16,47 @@ register("chat", (message, event) => {
         const lootNum = parseFloat(match[1]);
         const multiplier = parseFloat(match[2]);
         const roundedMultiplier = (multiplier * 100).toFixed(2);
-        zealotsSinceEye = zealotsSinceEye + 1
-        if (lootNum>persistentData.LootNumLower) {
-          //do nothing :D feel free to edit if you want!
-        } else if (lootNum<=persistentData.LootNumLower) {
-          ChatLib.chat("&f[&6DSA&f] &a♣&c" + lootNum + "  &6(&b✯" + roundedMultiplier + "&6)")
+        if (dragLootNumCheck && dragLootNumHiderOverride) {
+          dragLootNumCheck = false
+          ChatLib.chat("&f[&6DSA&f] &4[&cDragon&4] &a♣&c" + lootNum + "  &6(&b✯" + roundedMultiplier + "&6)")
+        } else {
+          zealotsSinceEye = zealotsSinceEye + 1
+          if (lootNum>Settings.lootNumLowerValue) {
+            //do nothing, lootnum value was too high
+          } else if (lootNum<=Settings.lootNumLowerValue) {
+            ChatLib.chat("&f[&6DSA&f] &a♣&c" + lootNum + "  &6(&b✯" + roundedMultiplier + "&6)")
+          }
         }
       }
+  } else if (message.includes("Your magic weapon hit") && Settings.magicWeaponHiderVar) {
+      cancel(event)
+      // do simplification here if I want later :D
+  } else if (message.includes("FIREBALL!!") && Settings.fireballVar) {
+      //fireball title
+      Client.showTitle("&cFireball","",0,40,5)
+      //entity.ender_dragon.name do stuff with this
   } else if (message.includes("Debug mode off.")) {
       ChatLib.say("/debug");
       cancel(event);
-      ChatLib.chat(`&f[&6DSA&f] &6Lootnum Hider set to ${persistentData.LootNumLower}`);
+      ChatLib.chat(`&f[&6DSA&f] &6Lootnum Hider set to ${(Settings.lootNumLowerValue).toString()}`);
   } else if (message.includes("Debug mode on.")) {
       cancel(event);
       ChatLib.chat("&f[&6DSA&f] &6Enabled Debug Mode!");
-  } else if (message.includes("RARE DROP! (Summoning Eye)")) {
+  } else if (message.includes("RARE DROP! (Summoning Eye)") && Settings.eyeTrackerVar) {
       cancel(event)
       new Message("&f[&6DSA&f] &6Summoning Eye took " + zealotsSinceEye + " Zealots. &c(&6±" + Uncertainty + "&c)").setChatLineId(5000).chat()
       Uncertainty = 0
       zealotsSinceEye = 0
   } else if (message.includes("Endstone Protector has spawned.") || (message.includes("Your Sleeping Eyes have been awoken by the magic of the dragon."))) {
       Uncertainty = Uncertainty + 1
-  } else if (message.includes("Backstab! Damage +150%.") && !persistentData.backstabStatus) {
+  } else if (message.includes("Backstab! Damage +150%.") && Settings.lividDaggerSpam) {
       cancel(event)
-  } else if (message.includes("Cleaning up mobs.")) {
-      if (Server.getIP().includes("dragsim.co")) {
-        ChatLib.say("/debug")
-      }
+  } else if (message.includes("Cleaning up mobs.") && Server.getIP().includes("dragsim.co") && Settings.autoDebugVar) {
+    ChatLib.say("/debug")
   };
 }).setCriteria("${message}");
 
 
 register("command", (...arguments) => { //this works fine, but vscode doesn't like '...arguments'
-  try {
-    const command = String(arguments[0]).toLowerCase();
-    switch (command) {
-      case "help":
-        ChatLib.chat("&f[&6DSA&f] &6Command Arguments");
-        ChatLib.chat(ChatLib.getChatBreak("&6-"));
-        ChatLib.chat("&f[&6DSA&f] /dsa ln <numxber> - Sets the highest lootnum to display (inclusive)");
-        ChatLib.chat("&f[&6DSA&f] /dsa backstab - toggles backstab message visibility");
-        ChatLib.chat(ChatLib.getChatBreak("&6-"));
-        break;
-      case "ln":
-        if (arguments.length < 2 || !/^\d+(\.\d+)?$/.test(arguments[1])) {
-          ChatLib.chat("&f[&6DSA&f] &cInvalid syntax! Use '/dsa help' for command help");
-        } else {
-          try {
-            persistentData.LootNumLower = parseFloat(arguments[1]);
-            persistentData.save()
-            ChatLib.chat(`&f[&6DSA&f] &6Won't display LootNum values above &b${persistentData.LootNumLower}`);
-          } catch (err) {
-            ChatLib.chat("&f[&6DSA&f] &cInvalid syntax! Use '/dsa help' for command help");
-          }
-        }
-        break;
-      case "backstab":
-        persistentData.backstabStatus = !persistentData.backstabStatus
-        persistentData.save()
-        break;
-      default:
-        ChatLib.chat("&f[&6DSA&f] &cInvalid syntax! Use '/dsa help' for command help");
-    }
-  } catch (err) {
-    ChatLib.chat("&f[&6DSA&f] &cInvalid syntax! Use '/dsa help' for command help");
-  }
+  Settings.openGUI()
 }).setName("dsa").setAliases("dragsimaddons");
